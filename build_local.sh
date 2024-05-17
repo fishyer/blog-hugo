4e2024a78c7bb57dcb2a6e32a71d91fd0dfcf799
fishyer <yutianran666@gmail.com>
2024-05-17 17:32:22 +0800

    update blog at 2024-05-17 17:32:21

--- build_local.sh
+++ build_local.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-cp -rf /Users/yutianran/Documents/MyPKM/note/blog/* /Users/yutianran/MyGithub/blog-hugo/content/
-cd /Users/yutianran/MyGithub/blog-hugo
+function log() {
+  echo [$(date +"%Y-%m-%d %H:%M:%S")] "$@"  | tee -a "$0.log"
+}
+
+function end(){
+    log "##################################################"
+}
+
+
+log "Running blog_hugo build_local.sh"
+log "Current datetime: $(date +"%Y-%m-%d %H:%M:%S")"
+
+cp -rf /Users/yutianran/Documents/MyNote/blog/* /Users/yutianran/Documents/MyCode/blog-hugo/content/
+cd /Users/yutianran/Documents/MyCode/blog-hugo
 hugo --buildFuture
-hugo server
+pkill -f "hugo server"
+nohup hugo server >> "$0.log" 2>&1 &
+log "blog server is run at http://localhost:1313/"
+
+end
--- build_server.sh
+++ build_server.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-cp -rf /Users/yutianran/Documents/MyPKM/note/blog/* /Users/yutianran/MyGithub/blog-hugo/content/
-cd /Users/yutianran/MyGithub/blog-hugo
-hugo --buildFuture
+source /Users/yutianran/Documents/MyCode/blog-hugo/build_local.sh
+cd /Users/yutianran/Documents/MyCode/blog-hugo
 git add .
 cur_time=$(date +'%Y-%m-%d %H:%M:%S')
 git commit -m "update blog at $cur_time"
 git push -u origin fishyer
-# hugo server
 echo "blog updated at $cur_time"
 echo "blog deployed successfully"
 echo "Please visit https://blog.fishyer.com"
